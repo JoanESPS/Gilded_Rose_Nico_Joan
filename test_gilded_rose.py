7c8fcda11c69d1481fb6e8dedbf8c7d9aec46971
JoanE <joan.espias@gmail.com>
2023-03-03 10:33:29 +0100

    correction de test_foo

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,9 +5,12 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
+        #Arrange
         items = [Item("foo", 0, 0)]
+        #Act
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
+        #Assert
         self.assertEqual("foo", items[0].name)
 
 
