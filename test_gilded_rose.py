850bd0ad9d30ea6777137eb4c30081506ac626fd
JoanE <joan.espias@gmail.com>
2023-03-03 10:43:42 +0100

    ajout test_item_sell_in_ko et refacto tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -6,21 +6,24 @@ from gilded_rose import Item, GildedRose
 class GildedRoseTest(unittest.TestCase):
     def test_item_name(self):
         # Arrange
-        items = [Item("foo", 0, 0)]
+        item = Item("foo", 0, 0)
         # Act
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
         # Assert
-        self.assertEqual("foo", items[0].name)
+        self.assertEqual("foo", item.name)
 
     def test_item_sell_in(self):
         # Arrange
-        items = [Item("foo", 10, 0)]
+        item = Item("foo", 10, 0)
         # Act
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
         # Assert
-        self.assertEqual("foo", items[0].name)
+        self.assertEqual(10, item.sell_in)
+
+    def test_item_sell_in_ko(self):
+        # Arrange
+        item = Item("foo", -1, 0)
+        # Act
+        # Assert
+        self.assertEqual(-1, item.sell_in)
 
 
 if __name__ == '__main__':
